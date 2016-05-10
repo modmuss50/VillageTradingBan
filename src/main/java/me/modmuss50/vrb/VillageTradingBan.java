03eed513b2ffdcee5eddd782746fed7a206a4f65
modmuss50 <modmuss50@gmail.com>
2016-05-10 08:26:24 +0100

    Fix major syncing issue

--- src/main/java/me/modmuss50/vrb/VillageTradingBan.java
+++ src/main/java/me/modmuss50/vrb/VillageTradingBan.java
@@ -2,16 +2,24 @@ package me.modmuss50.vrb;
 
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
-import cpw.mods.fml.relauncher.Side;
-import cpw.mods.fml.relauncher.SideOnly;
+import cpw.mods.fml.common.eventhandler.SubscribeEvent;
+import net.minecraft.entity.passive.EntityVillager;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.entity.player.EntityInteractEvent;
 
 @Mod(modid = "vtb", name = "VillageTradingBan", version = "1.0.0")
 public class VillageTradingBan {
 
-    @SideOnly(Side.CLIENT)
     @Mod.EventHandler
     public void init(FMLInitializationEvent event) {
-        new VillageTradingBanClient().load();
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    @SubscribeEvent
+    public void interact(EntityInteractEvent entityInteractEvent){
+        if(entityInteractEvent.target instanceof EntityVillager){
+            entityInteractEvent.setCanceled(true);
+        }
     }
 
 }
